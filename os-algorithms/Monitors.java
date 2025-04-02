89aebc6f528b211d6c7a070106606cc4adcb3b91
abeni-al7 <abenezeralebachew3@gmail.com>
2025-04-02 20:18:18 +0300

    Complete the whole thing

--- os-algorithms/Monitors.java
+++ os-algorithms/Monitors.java
@@ -1,3 +1,48 @@
 public class Monitors {
-    
+    private boolean inUse = false;
+
+    public synchronized void enter() throws InterruptedException {
+        while (inUse) {
+            wait();
+        }
+        inUse = true;
+    }
+
+    public synchronized void exit() {
+        inUse = false;
+
+        notifyAll();
+    }
+
+    public void performTask(String threadName) {
+        try {
+            enter();
+            System.out.println(threadName + " is entering the critical section.");
+            Thread.sleep(1000);
+            System.out.println(threadName + " is leaving the critical section.");
+            exit();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println(threadName + " was interrupted.");
+        }
+    }
+
+    public static void main(String[] args) {
+        final Monitors monitor = new Monitors();
+
+        Runnable task = () -> {
+            String threadName = Thread.currentThread().getName();
+            for (int i = 0; i < 3; i++) {
+                monitor.performTask(threadName);
+            }
+        };
+
+        Thread t1 = new Thread(task, "Thread-1");
+        Thread t2 = new Thread(task, "Thread-2");
+        Thread t3 = new Thread(task, "Thread-3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
 }
--- os-algorithms/Semaphores.java
+++ os-algorithms/Semaphores.java
@@ -1,3 +1,50 @@
 public class Semaphores {
-    
+
+    // A simple counting semaphore
+    public static class Semaphore {
+        private int permits;
+
+        public Semaphore(int permits) {
+            if (permits < 0) {
+                throw new IllegalArgumentException("Permit count cannot be negative.");
+            }
+            this.permits = permits;
+        }
+
+        public synchronized void acquire() throws InterruptedException {
+            while (permits == 0) {
+                wait();
+            }
+            permits--;
+        }
+
+        public synchronized void release() {
+            permits++;
+            notifyAll();
+        }
+    }
+
+    public static void main(String[] args) {
+        final Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 0; i < 10; i++) {
+            final int threadId = i;
+            new Thread(() -> {
+                try {
+                    System.out.println("Thread " + threadId + " waiting for a permit.");
+                    semaphore.acquire();
+                    System.out.println("Thread " + threadId + " acquired a permit.");
+                    
+                    // critical section work.
+                    Thread.sleep(1000);
+                    
+                    System.out.println("Thread " + threadId + " releasing a permit.");
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    System.out.println("Thread " + threadId + " was interrupted.");
+                }
+            }).start();
+        }
+    }
 }
